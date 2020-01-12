67ff71f0bef13d6416bf18dcb68123a143e25e74
Yuuki Wesp <ls-micro@ya.ru>
2020-01-12 22:35:51 +0300

    stage files

---
+++ src/IServiceProviderExtensions.cs
@@ -0,0 +1,38 @@
+﻿namespace Microsoft.Extensions.DependencyInjection
+{
+    using System;
+    using System.Linq;
+    using System.Reflection;
+
+    public static class IServiceProviderExtensions
+    {
+        public static void InjectFor<T>(this IServiceProvider provider, T obj)
+            => InjectFor(provider, (object)obj);
+        public static void InjectFor(this IServiceProvider provider, object obj)
+        {
+            var _injectablePropertyBindingFlags
+                = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic;
+            var type = obj.GetType();
+            var injectableProperties =
+                MemberAssignmentStatic.GetPropertiesIncludingInherited(obj.GetType(), _injectablePropertyBindingFlags);
+
+            injectableProperties = injectableProperties.Where(p => p.IsDefined(typeof(InjectAttribute)));
+
+            var tableOfInjectionData = injectableProperties.Select(property =>
+            (
+                propertyName: property.Name,
+                propertyType: property.PropertyType,
+                setter: MemberAssignmentStatic.CreatePropertySetter(obj.GetType(), property, false)
+            )).ToArray();
+
+            foreach (var (propertyName, propertyType, setter) in tableOfInjectionData)
+            {
+                var serviceInstance = provider.GetService(propertyType);
+                if (serviceInstance == null)
+                    throw new InvalidOperationException(
+                        $"Cannot provide a value for property '{propertyName}' on type '{type.FullName}'. There is no registered service of type '{propertyType}'.");
+                setter.SetValue(obj, serviceInstance);
+            }
+        }
+    }
+}
---
+++ src/InjectAttribute.cs
@@ -0,0 +1,9 @@
+﻿namespace Microsoft.Extensions.DependencyInjection
+{
+    using System;
+    // maybe conflict with Microsoft.AspNetCore.Components.InjectAttribute 
+    [AttributeUsage(AttributeTargets.Property)]
+    public sealed class InjectAttribute : Attribute
+    {
+    }
+}
